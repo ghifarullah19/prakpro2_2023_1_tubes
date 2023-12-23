5ddcaa6e4031cbde7d5412864645cd909a5e78a8
Muhammad Lutfi Amin Ghifarullah <ghifarullah11@gmail.com>
2023-12-23 07:10:12 +0700

    Commit Pertama

---
+++ src/main/java/id/ac/unpas/tech/in/TechIn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.ac.unpas.tech.in;
+
+/**
+ *
+ * @author ghifarullah19
+ */
+public class TechIn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
